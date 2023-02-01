d25f63bb0bf3c1ee671216437d71906f3bea7ffd
chesedo <pieter@chesedo.me>
2023-02-01 10:41:00 +0200

    feat: wrap around spawnSycn to handle errors

---
+++ helpers/process.ts
@@ -0,0 +1,48 @@
+import { spawnSync as _spawnSync, SpawnSyncOptions } from "child_process"
+
+// The error type returned from spawnSync has some extra fields we want to read
+interface SpawnError extends Error {
+    code: string
+}
+
+/**
+ *  Wrapper around `child_process.spawnSync` to safely handle any errors
+ */
+export function execSync(
+    command: string,
+    args?: ReadonlyArray<string>,
+    options?: SpawnSyncOptions
+): string | Buffer {
+    // Capture stderr (pipe) to handle errors
+    options = { ...options, ...{ stdio: ["inherit", "inherit", "pipe"] } }
+    let result = _spawnSync(command, args, options)
+
+    if (result.error) {
+        let error = result.error as SpawnError
+
+        if (error.code == "ENOENT") {
+            throw {
+                error: `Could not find command "${command}"`,
+            }
+        } else {
+            throw {
+                error,
+            }
+        }
+    }
+
+    if (result.status != 0) {
+        let problems = []
+
+        if (result.stderr) {
+            problems.push(result.stderr)
+        }
+
+        throw {
+            error: `Failed to execute command "${command}"`,
+            problems,
+        }
+    }
+
+    return result.stdout
+}
