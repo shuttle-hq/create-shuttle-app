aa00873103e3072968a48257975862cc2fa3cb4d
Pieter <pieter@chesedo.me>
2023-02-03 12:01:19 +0200

    feat: concurrently for local runs (#8)

--- helpers/package.ts
+++ helpers/package.ts
@@ -1,5 +1,6 @@
 import { readFileSync, writeFileSync } from "fs"
 import path from "path"
+import { execSync } from "./process"
 
 /**
  * Fixup `package.json` with some scripts for easier development
@@ -17,7 +18,42 @@ export function patchPackage(projectPath: string) {
         "cargo shuttle project new --working-directory ./backend/"
     packages["scripts"]["deploy"] =
         "build && cargo shuttle deploy --working-directory ./backend/"
+    packages["scripts"]["dev"] =
+        "concurrently --names \"next, shuttle\" --kill-others \"next dev\" \"cargo shuttle run --working-directory ./backend/\""
 
     let newData = JSON.stringify(packages, null, 4)
     writeFileSync(packagesPath, newData)
+
+    // Install using native package manager
+    let packageManager = getPkgManager()
+    let args = []
+
+    if (packageManager === "yarn") {
+        args.push("add", "--dev")
+    } else {
+        args.push("install", "--save-dev")
+    }
+
+    args.push("concurrently")
+    execSync(packageManager, args, { cwd: projectPath })
+}
+
+// Shamelessly copied, because ncc means I cannot access it from create-next-app even though it's a dev dependency, from
+// https://github.com/vercel/next.js/blob/fe2d26fcf41de7a749b8e8d2d89e3a5924f327bf/packages/create-next-app/helpers/get-pkg-manager.ts
+type PackageManager = 'npm' | 'pnpm' | 'yarn'
+
+function getPkgManager(): PackageManager {
+    const userAgent = process.env.npm_config_user_agent
+
+    if (userAgent) {
+        if (userAgent.startsWith('yarn')) {
+            return 'yarn'
+        } else if (userAgent.startsWith('pnpm')) {
+            return 'pnpm'
+        } else {
+            return 'npm'
+        }
+    } else {
+        return 'npm'
+    }
 }
