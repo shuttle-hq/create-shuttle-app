18a0ee0c2623b13fdfea8a968947a8d8b1a111cf
Pieter <pieter@chesedo.me>
2023-05-05 19:11:48 +0200

    misc: rename examples to shuttle-examples (#29)

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -11,4 +11,4 @@ export const SHUTTLE_MAC_TARGET = `x86_64-apple-darwin`
 export const SHUTTLE_WINDOWS_TARGET = `x86_64-pc-windows-msvc`
 
 export const SHUTTLE_EXAMPLE_URL =
-    "https://github.com/shuttle-hq/examples/axum/static-next-server"
+    "https://github.com/shuttle-hq/shuttle-examples/axum/static-next-server"
