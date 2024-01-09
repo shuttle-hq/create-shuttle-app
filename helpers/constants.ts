a46120907fae52e768265b015aa8aaa0ed275783
oddgrd <29732646+oddgrd@users.noreply.github.com>
2024-01-09 15:27:38 +0100

    chore: v0.1.38

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,6 +1,6 @@
 // The minimum required version of rust.
 export const RUSTC_VERSION = "1.74.0"
-export const SHUTTLE_VERSION = "0.34.0"
+export const SHUTTLE_VERSION = "0.35.0"
 
 export const SHUTTLE_TAG = `v${SHUTTLE_VERSION}`
 export const SHUTTLE_DOWNLOAD_URL = `https://github.com/shuttle-hq/shuttle/releases/download/${SHUTTLE_TAG}/`
